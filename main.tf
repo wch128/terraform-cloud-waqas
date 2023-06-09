17125edd61ff1d7c4f79600148a50191d9766647
Waqas Arif <107773389+wch128@users.noreply.github.com>
2023-06-09 15:18:20 +0500

    first commit in workspace2

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
-module "child_module" {
-  source            = "./childmodule"
-  repo_name         = var.repo_name
-  repo_description  = var.repo_description
-  branch_name       = var.branch_name
-  github_token      = var.github_token
+data "terraform_remote_state" "Terraform-cloud-waqas-algo" {
+  backend = "remote"
+
+  config = {
+    organization = "Waqas-algo"
+    workspaces = {
+      name = "Terraform-cloud-waqas-algo"
+    }
+  }
 }
 
-
-
+# Create a new workspace2-specific repository branch
+resource "github_branch" "branch" {
+  repository = data.terraform_remote_state.Terraform-cloud-waqas-algo.outputs.repo_name
+  name       = "my-branch-in-workspace2"
+  
+}
--- output.tf
+++
@@ -1,8 +0,0 @@
-# Expose repository and branch information as outputs
-output "repo_name" {
-  value = github_repository.repo.name
-}
-
-output "branch_name" {
-  value = github_branch.branch.name
-}
